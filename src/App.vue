a9bf411cbfe068f45d81b9b9e726833c55145bb7
Maxwell Cofie <maxwell@hubtel.com>
2023-01-07 17:15:05 +0000

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+      <button class="btn btn-primary">Stest</button>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: {}
 }
 </script>
 
